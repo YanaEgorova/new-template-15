fd55558c16f1e415073cea90b9f90bad9111a87c
Yana <yanaegorova69.3@gmail.com>
2021-08-28 20:42:10 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,163 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Yarn Bowl',
+        price: 6.96,
+        image: './img/img7.png',
+        description: [
+            {
+                text: 'Is it a project bag? A yarn bowl? It\'s both!',
+            },
+            {
+                text: 'Closed, your yarn and hook or needles are protected, secure and tidy, ready to be tucked away in a bag, a briefcase, or conveniently looped over your wrist.',
+            },
+            {
+                text: 'Open, it becomes a sturdy bowl to keep your yarns from twisting or rolling away as you work.',
+            },
+            {
+                text: 'Made of ultra-lightweight, flexible silicon, there is nothing to snag or damage your project.',
+            },
+            {
+                text: 'Available in a choice of three jewel shades, with a whimsical design of knitting stitches, this innovative project caddy takes your knit and crochet experience to the next level.',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Elastic Band Fishing Headwear',
+        price: 29.99,
+        discount: 24.65,
+        image: './img/img5.png',
+        description: [
+            {
+                title: 'Unique design',
+                text: 'This fun umbrella hat use very little space for portable travel. Stylish and functional accessories, practical design keeps you dry while keeping your hands free. If you don\'t know whether the hat umbrella is suitable for you, please buy it according to the size we provide.',
+            },
+            {
+                title: 'Function',
+                text: 'Oxford cloth top bounces light off so don\'t absorbed heat and anti-UV, which keeps you much cooler with the sun off your head. 8 metal ribs makes umbrella very strong, and in rainy weather, it was completely against the wind.',
+            },
+            {
+                title: 'Occasion',
+                text: 'Great for parties or other fun family outings. Wear this umbrella hat in the rain or shine. Suitable for gardening, hiking, farming, golfing and Halloween costume etc. This hat is a great personal sunshade at the beach or for fishing Too.',
+            },
+            {
+                title: 'Fit most',
+                text: 'This hat features an adjustable strap, small enough to fit a child\'s head but stretches big enough to fit a large adult head. Adjustable stape make umbrella hat tightly in place on your head. Which keep the colorful hat on your head comfortably even in the wind.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.jpg' , './img/img5-3.png', './img/img5-4.jpg', './img/img5-5.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Fitness Pushup',
+        price: 64.90,
+        discount: 69.99,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Pushup stand Designed to rotate slightly, engaging more muscles and increasing strength and definition in arms, shoulders, chest, back and abs.',
+            },
+            {
+                text: 'Ergonomic grips help distribute weight evenly, reducing pressure points and joint strain on wrists and elbows.',
+            },
+            {
+                text: 'Built to last construction with smooth rotation and steel ball bearing system; Treads on bottom of handles Securely grip all floor surfaces.',
+            },
+            {
+                text: 'Weight Vest Ready, 400 Pound weight Capacity.',
+            },
+            {
+                text: 'Dimensions: 10 inches x 8.25 inches x 6.2 inches; weight: 4 pounds.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Dry Food Dispenser',
+        price: 98.90,
+        discount: 55.97,
+        image: './img/img10.png',
+        description: [
+            {
+                text: 'Heavy-duty for commercial use but also suitable for the everyday kitchen.',
+            },
+            {
+                text: 'Designed to preserve freshness up to 45 days.',
+            },
+            {
+                text: 'Each canister holds up to 17.5 ounces of cereal or dried goods.',
+            },
+            {
+                text: 'Dimensions: 6.5"L x 7"W x 15"H',
+            },
+            {
+                text: 'Dry-food dispenser with 17-1/2-ounce scratch-resistant canister.',
+            },
+            {
+                text: 'Can hold a full box of cereal; preserves freshness for up to 45 days.',
+            },
+            {
+                text: 'Easy-turn dispensing knob; portion-control system; won\'t crush cereal.',
+            },
+        ],
+        images: ['./img/img10-1.png', './img/img10-2.png', './img/img10-3.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Glass LED aquarium Kit',
+        price: 119.98,
+        discount: 79.93,
+        image: './img/img9.png',
+        description: [
+            {
+                title: 'Sleek design',
+                text: 'Rounded corners and clear glass canopy allow viewing from multiple angles.',
+            },
+            {
+                title: 'Daylight/moonlight lighting',
+                text: 'Bright white LEDs create a shimmering sunlight effect; blue LEDs produce a moonlit glow.',
+            },
+            {
+                title: 'Easy access',
+                text: 'Hinged LED lighting and sliding glass canopy.',
+            },
+            {
+                title: 'Size',
+                text: '5-gallon aquarium.',
+            },
+            {
+                title: 'Hidden filtration',
+                text: 'Advanced, 3-stage filtration is out of sight, enhancing aquarium viewing.',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.png', './img/img9-3.png', './img/img9-4.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Craft Station',
+        price: 148.00,
+        discount: 95.50,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Station - Main Work Surface 36 x 24, craft desk large enough for all your art & craft needs.',
+            },
+            {
+                text: 'Four side trays for paints, brushes, pens, and other Accessories.',
+            },
+            {
+                text: 'Metal Frame holder with drip tray for glue gun and cup holder.',
+            },
+            {
+                text: 'Adjustable Desk - Finger touch adjustable angle top, top angle adjusts to 40 degrees, can be used as a drafting table, computer table, writing table, home hobby table or art & craft desk.',
+            },
+            {
+                text: 'Modern Desk - 3 Plastic Molded Slide Out Drawers for Storage which can be mounted on left or right side of table, handy desk storage.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png', './img/img2-4.jpg', './img/img2-5.png'],
+    },
+]
